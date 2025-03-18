7fafb7ccaf8b5227dd5dfb5933a81e85e2466fa2
magento2Ext <36661813+magento2Ext@users.noreply.github.com>
2025-03-18 11:59:08 +0530

    Folder updated

---
+++ js/sliders.js
@@ -0,0 +1,24 @@
+
+$('#slider_project').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
